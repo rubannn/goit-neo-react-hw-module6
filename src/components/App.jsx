6476d297a8d68e574effcebdc7a22aa1624b4d81
m.ruban <m.ruban@testportal.gov.ua>
2025-12-09 16:07:07 +0100

    defaultContacts initialization

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,13 +5,22 @@ import ContactForm from "./ContactForm/ContactForm";
 import ContactList from "./ContactList/ContactList";
 import SearchBox from "./SearchBox/SearchBox";
 
+
+const defaultContacts = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+];
+
+
 const getItemsFromStorage = () => {
   const contacts = localStorage.getItem("contacts");
   if (contacts) {
     return JSON.parse(contacts);
   }
-
-  return [];
+  localStorage.setItem("contacts", JSON.stringify(defaultContacts));
+  return defaultContacts;
 }
 
 const setItemsToStorage = (items) => {
